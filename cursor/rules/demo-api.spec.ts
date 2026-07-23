12a21c54cef8820aa619897c0a0994add2948bf1
Atthaboon Sanurt (P'Art) <atthaboon.s@qahive.com>
2026-07-23 15:51:14 +0700

    Update demo-api.spec.ts

--- cursor/rules/demo-api.spec.ts
+++ cursor/rules/demo-api.spec.ts
@@ -1,7 +1,27 @@
-import test from "@playwright/test";
+import test, { expect } from "@playwright/test";
 
 // https://demoqa.com/books
-
 test('GET API Request', async ({ request }) => {
   const resp = await request.get('https://demoqa.com/BookStore/v1/Books');
+  expect(resp.status()).toBe(200);
+
+  const body = await resp.json();
+  expect(body).toHaveProperty('books');
+  expect(body.books.length).toEqual(8);
 });
+
+test('POST API Request - Login', async ({ request }) => {
+  const response = await request.post(`https://demoqa.com/Account/v1/Login`, {
+    data: {
+      userName: 'qademo1001',
+      password: 'T@stpw120',
+    },
+  });
+  expect(response.status()).toBe(200);
+  
+  const body = await response.json();
+  expect(body).toHaveProperty('userId');
+  expect(body).toHaveProperty('token');
+});
+
+
